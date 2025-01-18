a92958ff1f034ac8be7cfcd559d5b6e9fdfd2807
Jef <jdovalov@gmail.com>
2025-01-18 14:02:24 -0500

    Fix PyLint complaints

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
+"""
+Emotion Detection web server
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route('/emotionDetector')
-def RunSentimentAnalysis():
+def run_sentiment_analysis():
+    """
+    Run the sentiment analysis on the string the user entered.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    return f"For the given satement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
+    return ( f"For the given satement, the system response is "
+    f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+    f"'fear': {response['fear']}, 'joy': {response['joy']}, "
+    f" and 'sadness': {response['sadness']}. "
+    f"The dominant emotion is {response['dominant_emotion']}" )
 
 @app.route('/')
 def render_index():
+    """
+    Render the index webpage
+    """
     return render_template("index.html")
 
 if __name__=="__main__":
